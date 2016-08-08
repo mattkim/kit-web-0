f541786b7c71ef1a8972ff608a7b5fb67285c042
Matt Kim <matthkim@gmail.com>
2016-08-07 20:15:02 -0700

    Adding a better error message when location is unavailable

--- src/components/Address/Address.js
+++ src/components/Address/Address.js
@@ -29,16 +29,10 @@ class Address extends Component {
 
       switch (error.code) {
         case error.PERMISSION_DENIED:
-          errorMessage = 'User denied the request for Geolocation.';
-          break;
-        case error.POSITION_UNAVAILABLE:
-          errorMessage = 'Location information is unavailable.';
-          break;
-        case error.TIMEOUT:
-          errorMessage = 'The request to get user location timed out.';
+          errorMessage = 'Please enable location services.';
           break;
         default:
-          errorMessage = 'An unknown error occurred.';
+          errorMessage = 'Location information is unavailable.  Please try again later.';
       }
 
       return (
